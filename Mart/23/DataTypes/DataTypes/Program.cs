51130a36d63a745d51f1d8bad2464f5a6f1a08c4
Firudin Qadimaliyev <parkurfirudin@gmail.com>
2022-03-23 13:05:11 -0700

    data tipleri

---
+++ Mart/23/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int reqem1 = 20;
+            //string yazi1 = "Salam xos gelmisiniz. Necesiz?";
+
+            //Console.WriteLine(yazi1);
+            //Console.WriteLine(yazi1);
+            //Console.WriteLine(yazi1);
+
+            //yazi1 = "Mesaj burada deyisdirildi";
+
+            //Console.WriteLine(yazi1);
+            //Console.WriteLine(yazi1);
+
+            //Console.WriteLine(reqem1);
+            //Console.WriteLine(reqem1);
+            //Console.WriteLine(reqem1);
+
+            //reqem1 = 35;
+            //reqem1 = 22;
+            //Console.WriteLine(reqem1);
+
+            //string name = "Firudin";
+            //string surname = "Qadımaliyev";
+            //Console.WriteLine(name + " " + surname);
+
+            //int reqem0 = 0;
+            //int reqem1 = 1;
+            //int reqem2 = 2;
+            //int regem3 = 3;
+            //int reqem4 = 4;
+            //int reqem5 = 5;
+            //int reqem6 = 6;
+            //int reqem7 = 7;
+            //int reqem8 = 8;
+            //int reqem9 = 9;
+            //int reqem10 = 10;
+            //Console.WriteLine(reqem0);
+            //Console.WriteLine(reqem1);
+            //Console.WriteLine(reqem2);
+            //Console.WriteLine(regem3);
+            //Console.WriteLine(reqem4);
+            //Console.WriteLine(reqem5);
+            //Console.WriteLine(reqem6);
+            //Console.WriteLine(reqem7);
+            //Console.WriteLine(reqem8);
+            //Console.WriteLine(reqem9);
+            //Console.WriteLine(reqem10);
+
+            //Console.WriteLine("Salam xos gelmisiniz!");
+
+            //Console.WriteLine("Adiniz nedir?");
+            //string ad = Console.ReadLine();
+
+            //Console.WriteLine("Soyadiniz nedir?");
+            //string soyad = Console.ReadLine();
+
+            //Console.WriteLine("neçe yaşınız var");
+            //string yas = Console.ReadLine();
+
+            //Console.WriteLine("Hansi ölkede yasiyirsiniz?");
+            //string olke = Console.ReadLine();
+
+            //Console.WriteLine("Hansi seherde yasiyirsiniz?");
+            //string seher = Console.ReadLine();
+
+
+            //Console.WriteLine(ad + " " + soyad + " " + olke + "nin " + seher + " seherinde yasayir. Onun " + yas + " yası var.");
+
+
+            Console.WriteLine("neçe mertebelı binada yaşıyırsan");
+            int mertebe = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
