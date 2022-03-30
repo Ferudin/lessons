513b79b60967afb3841c68c2592e6f8e2cfc9ade
Firudin Qadimaliyev <parkurfirudin@gmail.com>
2022-03-30 09:57:39 -0700

    tapsırıq elave edildi

---
+++ Mart/27/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+
+            Console.WriteLine("Adınız nedir?");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Soyadınız nedir?");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("neçe yaşınız var?");
+            string yas = Console.ReadLine();
+
+            Console.WriteLine("hansı iş sayesinde işdiyirsiniz");
+            string ish = Console.ReadLine();
+
+            Console.WriteLine("Hansı ölkede yaşıyırsız?");
+            string olke = Console.ReadLine();
+
+            Console.WriteLine("Hansı şeherde yaşıyırsız?");
+            string seher = Console.ReadLine();
+
+            Console.WriteLine("hobbileriniz nedir?");
+            string hobbiler = Console.ReadLine();
+
+            Console.WriteLine("avtomobil süre bilirsiniz?");
+            string avtomobil = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Ad: " + ad);
+            Console.WriteLine("Soyad: " + soyad);
+
+            Console.WriteLine("yaş: " + yas);
+
+            Console.WriteLine("iş: " + ish);
+
+            Console.WriteLine("Olke: " + olke);
+
+            Console.WriteLine("şeher: " + seher);
+
+            Console.WriteLine("hobbiler: " + hobbiler);
+
+            Console.WriteLine("avtomobil: " + avtomobil);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
